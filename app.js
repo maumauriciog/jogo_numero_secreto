dec09c64c47db30a3df7352dc73a41b4cd6b8643
Task View <maumauriciog@hotmail.com>
2025-01-16 20:24:45 -0300

    mudando arquivos

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Querida Jéssica, escolha entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10');
 }
 
 exibirMensagemInicial();
@@ -27,7 +27,7 @@ function verificarChute() {
     let chute = document.querySelector('input').value;
     
     if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Jéssica, Você Acertou!');
+        exibirTextoNaTela('h1', 'Você Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
